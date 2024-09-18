f665c34a7ca428e330cbcd4843e94eff407b910c
Hasangitt <pingpong038759@gmail.com>
2024-09-18 22:15:04 +0600

    js

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -11,3 +11,55 @@
       mainSection.scrollIntoView({ behavior: 'smooth' });
     });
   });
+
+// ***************how seats left from  total**************
+
+  //   document.addEventListener("DOMContentLoaded", () => {
+  
+  //   let totalSeats = 40;  // Set total seats
+  //   const totalSeatsDisplay = document.getElementById("totalSeats");
+  // // Update the display with the initial total
+  //   totalSeatsDisplay.innerText = totalSeats;
+  
+  //   const seats = document.querySelectorAll(".seat");
+ 
+  //   // Add click event listener to each seat
+  //   seats.forEach(seat => {
+  //     seat.addEventListener("click", () => {
+  //       // Check if the seat is not already selected
+  //       if (!seat.classList.contains("selected")) {
+  //         seat.classList.add("selected");  // Add selected class (turns green)
+  //         totalSeats--;  // Decrease the total seat count
+  //       }
+  //      else {
+  //       // Deselect the seat (if allowed)
+  //       seat.classList.remove("selected");
+  //       totalSeats++;
+  //     }
+  //       // Update the total seat display
+  //       totalSeatsDisplay.textContent = totalSeats;
+  //     });
+  //   });
+  // });
+
+  document.addEventListener('DOMContentLoaded', function(){
+     let totalSeats = 40;
+     const totalSeatsDisplay = document.getElementById('totalSeats');
+     totalSeatsDisplay.innerText = totalSeats;
+
+     const seats = document.querySelectorAll(".seat");
+     seats.forEach(seat => {
+      seat.addEventListener('click', () => {
+        if(!seat.classList.contains('selected')){
+          seat.classList.add('selected');
+          totalSeats--;
+        }
+        else{
+          seat.classList.remove('selected');
+          totalSeats++;
+        }
+        totalSeatsDisplay.innerText = totalSeats;
+      });
+     });
+  });
+
